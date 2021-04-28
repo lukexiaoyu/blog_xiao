88392e8ff9a7624425e1f70c5a6978fe0bec84f7
klpalace <814008000@163.com>
2021-04-28 12:04:28 +0800

    1

--- deploy.sh
+++ deploy.sh
@@ -18,8 +18,8 @@ if [ -z "$GITHUB_TOKEN" ]; then
 else
   msg='来自github actions的自动部署'
   githubUrl=https://lukexiaoyu:${GITHUB_TOKEN}@github.com/lukexiaoyu/blog_xiao.git
-  git config --global user.name "klpalace"
-  git config --global user.email "814008000@163.com"
+  # git config --global user.name "lukexiaoyu"
+  # git config --global user.email "814008000@163.com"
 fi
 git init
 git add -A
