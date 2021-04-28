b94b7c720fa07b8202a3a6d1e189c982411271d4
klpalace <814008000@163.com>
2021-04-28 10:56:19 +0800

    first
    first

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+const head = require('./config/head.js');
+const plugins = require('./config/plugins.js');
+const themeConfig = require('./config/themeConfig.js');
+
+module.exports = {
+  theme: 'vdoing', // 使用依赖包主题
+  // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
+
+  title: "Luke's home",
+  description: '个人知识库',
+  base: '/blog_xiao/', // 格式：'/<仓库名>/'， 默认'/'
+  markdown: {
+    lineNumbers: true, // 代码行号
+  },
+
+  head,
+  plugins,
+  themeConfig,
+}
